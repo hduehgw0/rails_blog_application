04ba43eddd49e5deb4c185e4f5f385b6d25a4115
Kohei Nishiwaki <koheinishiwaki117@gmail.com>
2025-11-04 13:51:25 +0900

    feat: T-07 Implement User model validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # T-18 のアソシエーション (先行実装)
+  has_many :posts, dependent: :destroy
+
+  # (Relationshipのアソシエーションは T-18 で実装)
+
+  # バリデーション (仕様書 4.2)
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :introduction, length: { maximum: 200 }
 end
